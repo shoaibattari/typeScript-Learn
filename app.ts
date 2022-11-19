dcad10428ef5058183572caa3e38451a6d617917
shoaib memon <shoaibattari83@gmail.com>
2022-11-20 00:45:39 +0500

    first typescript code

---
+++ app.ts
@@ -0,0 +1,61 @@
+// let a = "hello world";
+// a = 'pak';
+// // a = 5;
+// console.log(a);
+
+
+// let stname:string = "shoaib";
+// let stage:number = 25;
+// let available:boolean = true;
+
+// console.log(stname,stage,available);
+// stname = 'sattar';
+// stage = 55;
+// available = false;
+
+// console.log(stname,stage,available);
+
+// let a = 'saleem';
+// let b = 25;
+// let c = true;
+// console.log(a,b,c);
+
+// a = 'dawood';
+// b = 26;
+// c = false;
+// // b =  'saleem'
+// console.log(a,b,c);
+
+
+
+// const a = 'salllam';
+// const b:number = 5;
+// const c= true;
+// console.log(a,b,c);
+
+
+// let age:number = 15;
+// age = 50;
+
+// if(age<19){
+//     console.log('you are teen age');
+// }else{
+//         console.log('yo are man');
+        
+//     }
+    
+
+// if (true) {
+// 	let z = 4;
+// console.log(z);
+
+//     z=66;
+// console.log(z);
+
+// }
+// else {
+// 	let z = "string";
+//     console.log(z);
+    
+// }
+// // console.log("let: ");
